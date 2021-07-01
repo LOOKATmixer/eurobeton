9542586cc02d46e068cf1e14e58390e3c343fa4e
LOOKATmixer <65297330+LOOKATmixer@users.noreply.github.com>
2021-07-01 11:06:43 +0500

    галерея и цены

---
+++ js/gallery.js
@@ -0,0 +1,31 @@
+if ($('.data__container').length != 0) {
+    $('.data__container').slick({
+        variableWidth: true,
+        prevArrow: '<div class="data__arrow data__arrow_prev"></div>',
+        nextArrow: '<div class="data__arrow data__arrow_next"></div>',
+        asNavFor: '.images__container',
+    });
+}
+
+if ($('.images__container').length != 0) {
+    $('.images__container').slick({
+        arrows: false,
+        variableWidth: true,
+        asNavFor: '.data__container',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    rows: 4,
+                    slideToShow: 1,
+                    centerMode: true,
+                }
+            },
+        ]
+    });
+}
+
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+
+});
