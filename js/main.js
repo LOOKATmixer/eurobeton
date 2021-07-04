7bcdcce9035cc9a4e9ac60a0774a926d8d0efda3
LOOKATmixer <65297330+LOOKATmixer@users.noreply.github.com>
2021-07-04 18:54:04 +0500

    плавность цифрам на мейне

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     $('.j-main__number').each(function () {
         var $this = $(this);
         jQuery({Counter: 0}).animate({Counter: $this.text()}, {
-            duration: 3000,
-            easing: 'swing',
+            duration: 5000,
+            easing: 'easeOutCirc',
             step: function () {
                 $this.text(Math.ceil(this.Counter));
             }
